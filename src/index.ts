723d599f45d65d383387d8ca2ef43b02f8d6023c
Lior Bashan <liorbashanx@gmail.com>
2021-11-05 17:55:19 +0200

    add xhr

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { QueryStringHandler } from "./queryStringHandler";
 import { LocalStorageHandler } from "./localStotageHandler";
+import { httpRequest } from "./http-request";
 
-const gclidList: string = "gclidList";
-const lsh: LocalStorageHandler = new LocalStorageHandler();
-const queryStringHandler: QueryStringHandler = new QueryStringHandler();
+(async () => {
+  async function init(): Promise<any> {
+    const postUrl: string = "http://localhost:3000/api/todos";
+    const gclidList: string = "gclidList";
+    const lsh: LocalStorageHandler = new LocalStorageHandler();
+    const queryStringHandler: QueryStringHandler = new QueryStringHandler();
 
-let searchParams: URLSearchParams = queryStringHandler.normalizeUrlSearchParams();
-const gclidValue: string | null = queryStringHandler.getGclidValue(searchParams);
-if (gclidValue) {
-  lsh.logGclid(gclidList, gclidValue);
-}
+    let searchParams: URLSearchParams = queryStringHandler.normalizeUrlSearchParams();
+    const gclidValue: string | null = queryStringHandler.getGclidValue(searchParams);
+    if (gclidValue) {
+      lsh.logGclid(gclidList, gclidValue);
+      await httpRequest("POST", postUrl, gclidValue).catch((error) => {
+        console.error(error);
+      });
+    }
+  }
+  init();
+})();
